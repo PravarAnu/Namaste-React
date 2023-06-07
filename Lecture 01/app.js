8e6f3e120ec6d3a0ce26446db61dffe8b17ebeca
Pravar Anu <pravaranu6@gmail.com>
2023-06-07 13:49:39 +0530

    More new things added in the app.js

--- Lecture 01/app.js
+++ Lecture 01/app.js
@@ -11,9 +11,36 @@ const heading = React.createElement("h1", { id: "heading"}, "Hello from React");
     ...
 */
 
+/* 
+    What if we want to create a html structure which look like this using react
+    <div id="parent">
+        <div id="child">
+            <h1>I am the h1 tag</h1>
+        </div>
+    </div>
+*/
+
+const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, React.createElement("h1", {}, "I am the h1 tag") ) );
+
+// console.log(parent);
+
+
+/* 
+    What if we want to add sibling of h1 means : 
+    <div id="parent">
+        <div id="child">
+            <h1>I am the h1 tag</h1>
+            <h2>I am the h2 tag</h2>
+        </div>
+    </div>
+*/
+const newParent = React.createElement("div", {id: "parent"}, React.createElement("div", {id:"child"}, [React.createElement("h1", {}, "I am the h1 tag"), React.createElement("h2", {}, "I am the h2 tag")]));
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// root.render(heading);
+root.render(newParent);
+
 
 /* 
     This root.render() function change the Heading (javascript object) into the html element with all its neccessary data.
